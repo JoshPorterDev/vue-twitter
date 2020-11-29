70194acce32232497d2693500d0156e96a91ad19
JoshPorterDev <porterjosh417@gmail.com>
2020-11-28 19:05:36 -0600

    updated right side bar

--- src/components/RightSideBar.vue
+++ src/components/RightSideBar.vue
@@ -4,8 +4,8 @@
       <h2>trending</h2>
       <h1 class="columnHeading">Who to follow</h1>
       <ul>
-          <h2>@realJoshPorter</h2>
-          <h2>@realDonaldTrump</h2>
+          <h2>@twitterhandle</h2>
+          <h2>@twitterhandle</h2>
       </ul>
   </div>
 </template>
